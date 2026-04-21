a41f4f7c6d358eb749a02a3ace50a3aa0bfdc427
Muhammad Atif <muhammadatif6541@gmail.com>
2026-04-21 18:20:09 +0500

    app.py fixed

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -12,12 +13,17 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    df = pd.DataFrame([data])
+    try:
+        data = request.json
+        df = pd.DataFrame([data])
 
-    prediction = model.predict(df)[0]
+        prediction = model.predict(df)[0]
 
-    return jsonify({'prediction': int(prediction)})
+        return jsonify({'prediction': int(prediction)})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
